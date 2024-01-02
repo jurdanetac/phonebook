d2aacb4f70b2e2e9b16b0533c1e79911f60cfe78
Juan Urdaneta <jurdanetac10@gmail.com>
2024-01-02 12:39:31 -0400

    Add data sent in request to logging

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+// create token that returns string of body data sent in post request
+morgan.token("data", (request, response) =>
+  request.method === "POST" ? JSON.stringify(request.body) : "",
+);
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data"),
+);
 
 let phonebook = [
   {
